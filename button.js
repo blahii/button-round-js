cd6f9619074174c8cb85e61c12caecdcc8849644
Dmytro Blahii <68820894+blahii@users.noreply.github.com>
2024-10-29 00:02:27 +0100

    Update button.js

--- button.js
+++ button.js
@@ -65,56 +65,96 @@ function drawButton() {
   drawingContext.shadowColor = "rgba(0, 0, 0, 0)";
 }
 
-function roundedPoly(ctx, points, radiusAll) {
-  let i, x, y, len, p1, p2, p3, v1, v2, sinA, sinA90, radDirection, drawDirection, angle, halfAngle, cRadius, lenOut, radius;
-  const asVec = (p, pp, v) => {
-    v.x = pp.x - p.x;
-    v.y = pp.y - p.y;
-    v.len = Math.sqrt(v.x * v.x + v.y * v.y);
-    v.nx = v.x / v.len;
-    v.ny = v.y / v.len;
-    v.ang = Math.atan2(v.ny, v.nx);
-  };
-  
-  radius = radiusAll;
-  v1 = {};
-  v2 = {};
-  len = points.length;
-  p1 = points[len - 1];
-  for (i = 0; i < len; i++) {
-    p2 = points[i % len];
-    p3 = points[(i + 1) % len];
-    asVec(p2, p1, v1);
-    asVec(p2, p3, v2);
-    sinA = v1.nx * v2.ny - v1.ny * v2.nx;
-    sinA90 = v1.nx * v2.nx - v1.ny * -v2.ny;
-    angle = Math.asin(sinA < -1 ? -1 : sinA > 1 ? 1 : sinA);
-    radDirection = 1;
-    drawDirection = false;
-    if (sinA90 < 0) {
-      angle = angle < 0 ? Math.PI + angle : Math.PI - angle;
-      radDirection = -1;
-      drawDirection = true;
-    } else if (angle > 0) {
-      radDirection = -1;
-      drawDirection = true;
-    } else {
-      angle = TAU + angle;
-    }
-    radius = p2.radius || radiusAll;
-    halfAngle = angle / 2;
-    lenOut = Math.abs((Math.cos(halfAngle) * radius) / Math.sin(halfAngle));
-    if (lenOut > Math.min(v1.len / 2, v2.len / 2)) {
-      lenOut = Math.min(v1.len / 2, v2.len / 2);
-      cRadius = Math.abs((lenOut * Math.sin(halfAngle)) / Math.cos(halfAngle));
-    } else {
-      cRadius = radius;
-    }
-    x = p2.x + v2.nx * lenOut + -v2.ny * cRadius * radDirection;
-    y = p2.y + v2.ny * lenOut + v2.nx * cRadius * radDirection;
-    ctx.arc(x, y, cRadius, v1.ang + (Math.PI / 2) * radDirection, v2.ang - (Math.PI / 2) * radDirection, drawDirection);
-    p1 = p2;
-    p2 = p3;
-  }
-  ctx.closePath();
+ function roundedPoly(ctx, points, radiusAll) {
+        var i,
+          x,
+          y,
+          len,
+          p1,
+          p2,
+          p3,
+          v1,
+          v2,
+          sinA,
+          sinA90,
+          radDirection,
+          drawDirection,
+          angle,
+          halfAngle,
+          cRadius,
+          lenOut,
+          radius;
+        var asVec = function (p, pp, v) {
+          v.x = pp.x - p.x;
+          v.y = pp.y - p.y;
+          v.len = Math.sqrt(v.x * v.x + v.y * v.y);
+          v.nx = v.x / v.len;
+          v.ny = v.y / v.len;
+          v.ang = Math.atan2(v.ny, v.nx);
+        };
+        radius = radiusAll;
+        v1 = {};
+        v2 = {};
+        len = points.length;
+        p1 = points[len - 1];
+        for (i = 0; i < len; i++) {
+          p2 = points[i % len];
+          p3 = points[(i + 1) % len];
+          asVec(p2, p1, v1);
+          asVec(p2, p3, v2);
+          sinA = v1.nx * v2.ny - v1.ny * v2.nx;
+          sinA90 = v1.nx * v2.nx - v1.ny * -v2.ny;
+          angle = Math.asin(sinA < -1 ? -1 : sinA > 1 ? 1 : sinA);
+          radDirection = 1;
+          drawDirection = false;
+          if (sinA90 < 0) {
+            if (angle < 0) {
+              angle = Math.PI + angle;
+            } else {
+              angle = Math.PI - angle;
+              radDirection = -1;
+              drawDirection = true;
+            }
+          } else {
+            if (angle > 0) {
+              radDirection = -1;
+              drawDirection = true;
+            } else {
+              angle = TAU + angle;
+            }
+          }
+          if (p2.radius !== undefined) {
+            radius = p2.radius;
+          } else {
+            radius = radiusAll;
+          }
+          halfAngle = angle / 2;
+          lenOut = Math.abs(
+            (Math.cos(halfAngle) * radius) / Math.sin(halfAngle)
+          );
+          if (lenOut > Math.min(v1.len / 2, v2.len / 2)) {
+            lenOut = Math.min(v1.len / 2, v2.len / 2);
+            cRadius = Math.abs(
+              (lenOut * Math.sin(halfAngle)) / Math.cos(halfAngle)
+            );
+          } else {
+            cRadius = radius;
+          }
+          x = p2.x + v2.nx * lenOut;
+          y = p2.y + v2.ny * lenOut;
+          x += -v2.ny * cRadius * radDirection;
+          y += v2.nx * cRadius * radDirection;
+          ctx.arc(
+            x,
+            y,
+            cRadius,
+            v1.ang + (Math.PI / 2) * radDirection,
+            v2.ang - (Math.PI / 2) * radDirection,
+            drawDirection
+          );
+          p1 = p2;
+          p2 = p3;
+        }
+        ctx.closePath();
+      }
 }
